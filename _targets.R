4f7b79fc0557a474b87393b3ebc4135c560bb493
Levin <slevin@usgs.gov>
2021-10-21 09:33:10 -0500

    created static branches for pipeline

--- _targets.R
+++ _targets.R
@@ -1,6 +1,7 @@
 
 library(targets)
 library(tarchetypes)
+library(tidyverse)
 
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval", "urbnmapr", "rnaturalearth", "cowplot"))
@@ -11,8 +12,9 @@ source("1_fetch/src/get_site_data.R")
 source("3_visualize/src/map_sites.R")
 
 # Configuration
-states <- c('WI','MN','MI')
+states <- c('WI','MN','MI','IL')
 parameter <- c('00060')
+
 
 # Targets
 list(
@@ -21,11 +23,10 @@ list(
 
   tar_map(
     values = tibble(state_abb = states),
-    tar_target(data, get_site_data(oldest_active_sites, state_abb, parameter))
+    tar_target(nwis_data, get_site_data(oldest_active_sites, state_abb, parameter))
     # Insert step for tallying data here
     # Insert step for plotting data here
   ),
-
 
   # Map oldest sites
   tar_target(
